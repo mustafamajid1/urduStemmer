5d200420104eed3e86db83fa8d81fca978d51244
mustafamajid1 <mustafa.majid@hotmail.com>
2019-04-07 01:08:09 +0400

    More rules and urdu words

--- urduProc.py
+++ urduProc.py
@@ -1,14 +1,21 @@
 strings = []
-with open(r"urdu.txt",encoding='utf-8') as f:
+with open(r"urdu.txt",encoding='utf-8') as f: #formatting and pushing into array
     for line in f:
         if line == '\ufeff\n':
             continue
-        strings.append(line.rstrip())
+        line = line.replace("\ufeff","")
+        strings.append(line.rstrip()) 
 
-print(strings)
+print("Array: ",strings)
 for contents in strings:
-    if contents[-2:] == "اں":
-        contents = contents[:-2]
-        print(contents)
+    if contents[-2:] == "اں": #regular plural
+        contents_new = contents[:-2]
+    elif contents[-4:] == "وانا":
+        contents_new = contents.replace("وا","") #verb, infinitive to direct causative infinitive
+    elif contents[-2:] == "گی":
+         contents_new = contents.replace("گی","") #suffix extraction
+    elif contents[0:2] == "بد":
+         contents_new = contents.replace("بد","")
+    print(contents," -> ",contents_new)
 
 
